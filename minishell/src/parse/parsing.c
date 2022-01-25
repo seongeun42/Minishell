6246e056af7a13fe1e8fdaf56ee773227c69b4c5
recicoco068 <gkapf838@gmail.com>
2022-01-26 02:13:15 +0900

    Modify parse functions

--- minishell/src/parse/parsing.c
+++ minishell/src/parse/parsing.c
@@ -17,31 +17,76 @@ t_list	*parsing(char *line, t_env* env_list)
 	t_list	*str;
 	t_list	*cmd;
 
-	// test code
+	// // test code
 
-	t_env* tmp;
+	// t_env* tmp;
 
-	tmp = (t_env *)malloc(sizeof(t_env) * 3);
-	tmp[0].key = "a";
-	tmp[0].value = "aaaa";
-	tmp[1].key = "b";
-	tmp[1].value = "hi";
-	tmp[2].key = 0;
-	tmp[2].value = 0;
-	str = ft_lstnew(0);
-	cmd = ft_lstnew(0);
-	line_split(line, env_list, &str);
-	// cmd 리스트 만들기
+	// tmp = (t_env *)malloc(sizeof(t_env) * 3);
+	// tmp[0].key = "a";
+	// tmp[0].value = "aaaa";
+	// tmp[1].key = "b";
+	// tmp[1].value = "hi";
+	// tmp[2].key = 0;
+	// tmp[2].value = 0;
+	// str = ft_lstnew(0);
+	// cmd = ft_lstnew(0);
+	// line_split(line, env_list, &str);
+	// // cmd 리스트 만들기
 
-	printf("str size: %d\n", ft_lstsize(str));
-	t_list *cur = str->next;
-	for (int i = 0; cur; i++)
-	{
-		printf("%d: %s\n", i, (char *)cur->content);
-		cur = cur->next;
-	}
+	// printf("str size: %d\n", ft_lstsize(str));
+	// t_list *cur = str->next;
+	// for (int i = 0; cur; i++)
+	// {
+	// 	printf("%d: %s\n", i, (char *)cur->content);
+	// 	cur = cur->next;
+	// }
 
 	return (cmd);
+}
+
+static void	line_split_flag_change(char c, t_flag* f)
+{
+	if (c == '"' && !f->bigq && !f->smallq)
+	{
+		f->bigq = 1;
+		f->space = 0;
+	}
+	else if (c == '"' && f->bigq)
+	{
+		f->bigq = 0;
+		f->space = 1;
+	}
+	else if (c == '\'' && !f->smallq && !f->bigq)
+	{
+		f->smallq = 1;
+		f->space = 0;
+	}
+	else if (c == '\'' && f->smallq)
+	{
+		f->smallq = 0;
+		f->space = 1;
+	}
+}
+
+
+static void	make_node_flag_change(char c, t_flag* f, char *value, int *i)
+{
+	if (c == '"' && !f->bigq && !f->smallq)
+		f->bigq = 1;
+	else if (c == '"' && f->bigq)
+		f->bigq = 0;
+	else if (c == '\'' && !f->smallq && !f->bigq)
+	{
+		f->smallq = 1;
+		f->env_chg = 0;
+	}
+	else if (c == '\'' && f->smallq)
+	{
+		f->smallq = 0;
+		f->env_chg = 1;
+	}
+	else
+		value[(*i)++] = c;
 }
 
 void	line_split(char *line, t_env *env_list, t_list **head)
@@ -59,74 +104,37 @@ void	line_split(char *line, t_env *env_list, t_list **head)
 	{
 		if (f.space && line[current] == ' ')
 		{
-			// make_new_node()로 새로운 노드 만들어서 str리스트에 연결
 			ft_lstadd_back(head, make_node(line, env_list, start, current));
 			while (line[current] == ' ')
 				++current;
 			start = current;
+			--current;
 		}
-		else if (line[current] == '"' && !f.bigq && !f.smallq)
-		{
-			f.bigq = 1;
-			f.space = 0;
-		}
-		else if (line[current] == '"' && f.bigq)
-		{
-			f.bigq = 0;
-			f.space = 1;
-		}
-		else if (line[current] == '\'' && !f.smallq && !f.bigq)
-		{
-			f.smallq = 1;
-			f.space = 0;
-		}
-		else if (line[current] == '\'' && f.smallq)
-		{
-			f.smallq = 0;
-			f.space = 1;
-		}
+		else if (line[current] == '"' || line[current] == '\'')
+			line_split_flag_change(line[current], &f);
 	}
-	ft_lstadd_back(head, make_node(line, env_list, start, current));
+	if (start != current)
+		ft_lstadd_back(head, make_node(line, env_list, start, current));
 }
 
-// 환경변수의 경우, 공백or"or'이 나올 때까지를 key로 봄
-// export ab=kk라고 했을 때
-// echo "abc  $abkk hi"
-// 라고 하면 [abc   hi]라고 출력됨
-// $abkk로 인식한 것
 t_list	*make_node(char *line, t_env *env_list, int start, int end)
 {
 	char	*value;
-	int		size;
 	int		i;
 	t_flag	f;
 
 	f = (t_flag){1, 1, 0, 0};
-	size = end - start;
-	value = (char *)malloc(sizeof(char) * (size + 1));
+	value = (char *)malloc(sizeof(char) * (end - start + 1));
 	if (!value)
 		return (0);
 	i = 0;
 	while (start < end)
 	{
 		if (f.env_chg && line[start] == '$')
-			value = env_substitude(value, env_find(line, env_list, &start), &i, size);
-		else if (line[start] == '"' && !f.bigq && !f.smallq)
-			f.bigq = 1;
-		else if (line[start] == '"' && f.bigq)
-			f.bigq = 0;
-		else if (line[start] == '\'' && !f.smallq && !f.bigq)
-		{
-			f.smallq = 1;
-			f.env_chg = 0;
-		}
-		else if (line[start] == '\'' && f.smallq)
-		{
-			f.smallq = 0;
-			f.env_chg = 1;
-		}
+			value = env_substitude(value,
+				env_find(line, env_list, &start), &i, end - start);
 		else
-			value[i++] = line[start];
+			make_node_flag_change(line[start], &f, value, &i);
 		++start;
 	}
 	value[i] = 0;
@@ -164,22 +172,20 @@ char	*env_find(char *line, t_env *env_list, int *start)
 char	*env_substitude(char *value, char *env, int *i, int size)
 {
 	char	*tmp;
-	char	*new_value;
+	char	*new_v;
 
 	tmp = env;
 	if (!tmp)
 		return (value);
 	else
 	{
-		new_value = (char *)malloc(sizeof(char) * (size + ft_strlen(env) + 1));
-		ft_strlcpy(new_value, value, *i + 1);
-		ft_strlcat(new_value, env, size + ft_strlen(env) * 2);
+		new_v = (char *)malloc(sizeof(char) * (size + ft_strlen(env) + 1));
+		ft_strlcpy(new_v, value, *i + 1);
+		ft_strlcat(new_v, env, size + ft_strlen(env) * 2);
 		*i += ft_strlen(env) + 1;
 		free(value);
-		// while (*tmp)
-		// 	value[(*i)++] = *tmp++;
 	}
-	return (new_value);
+	return (new_v);
 }
 
 char	*memory_fit(char *value)
