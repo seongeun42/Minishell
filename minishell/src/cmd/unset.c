f757e7faafe7213057324b4609db45accef1aa83
josuhee <69746967+josuhee@users.noreply.github.com>
2022-02-13 18:23:47 +0900

    unset complete

---
+++ minishell/src/cmd/unset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sujo <sujo@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/13 17:39:48 by sujo              #+#    #+#             */
+/*   Updated: 2022/02/13 18:08:00 by sujo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	unset(t_env *start, char **key)
+{
+	int idx;
+
+	if (key == NULL)
+		return ;
+	idx = 0;
+	while (key[idx])
+	{
+		printf("[key] : [%s]\n", key[idx]);
+		delete_env_node(&start, key[idx]);
+		idx++;
+	}
+}
