c737e7ac2d36c55bee09402bb044eaa7c1192d08
seongeun42 <42.4.seongele@gmail.com>
2022-03-13 17:54:39 +0900

    Add Redirect Execute

--- minishell/include/bulit_in.h
+++ minishell/include/bulit_in.h
@@ -3,14 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   bulit_in.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seongele <seongele@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: seongele <seongele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 16:09:45 by seongele          #+#    #+#             */
-/*   Updated: 2022/01/23 16:20:22 by seongele         ###   ########.fr       */
+/*   Updated: 2022/03/13 17:52:44 by seongele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BULIT_IN_H
 # define BULIT_IN_H
+
+// envp struct
+typedef struct s_env
+{
+	struct s_env	*next;
+	char			*key;
+	char			*value;
+}	t_env;
+
+// envp function
+int		env_parsing(char *envp[], t_env **list);
+char	**env_split(char const *s, char c);
+void	display_env(t_env *start);
+void	display_export(t_env *start);
+char	*search_env(t_env *start, char *key_);
+void	delete_env_node(t_env **start, char *key_);
+int		cmd_insert_env_node(t_env **env, char *str);
+t_env	*create_env_new_node(char *key_, char *value_);
+void	update_env_node(t_env *start, char *key_, char *value_);
+void	insert_new_env_node(t_env *start, t_env *new_node);
+
+// built in
+void	ft_unset(t_env *start, char **key);
+void	ft_env(t_env *start);
+int		ft_export(t_env *start, char **str);
+int		ft_pwd(void);
+int		ft_cd(t_env *start, char *path);
+int		ft_echo(char **cmd);
 
 #endif
