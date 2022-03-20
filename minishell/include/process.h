bf386314f25300748d79d0f43418382165a05dce
seongeun42 <42.4.seongele@gmail.com>
2022-03-20 13:13:42 +0900

    process.h

--- minishell/include/process.h
+++ minishell/include/process.h
@@ -6,7 +6,7 @@
 /*   By: seongele <seongele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 16:10:14 by seongele          #+#    #+#             */
-/*   Updated: 2022/03/13 17:19:48 by seongele         ###   ########.fr       */
+/*   Updated: 2022/03/20 13:13:22 by seongele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,12 @@ typedef s_cmd_flag
 }	t_cmd_flag;
 
 // process fuctions
+int	cmd_redirect_exec(t_list *cmd_head, t_list *redi_head, t_list *env);
+int	exec_process(char **cmd, t_list *redirect, t_list *env, t_cmd_flag *flag);
+int	child(char **cmd, t_list *redirect, t_list *env, int fd[]);
+int	change_stdinout(t_cmd_flag *flag, int fd[]);
+int	only_cmd(t_list *cmd, t_list *redi, t_list *env);
+
 
 // redirect execute fuctions
 int	exec_redirect(t_list *redirect);
